ad2989e6624bd43002460c8a585e9fa32bb9bf40
nealium413 <nealium413@gmail.com>
2025-02-01 03:54:44 +0000

    nextjs github pages update

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/joeymcpattern' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/joeymcpattern/' : '',
   images: {
     unoptimized: true,
-  }
+  },
+  trailingSlash: true,
+  reactStrictMode: true
 };
 
 export default nextConfig;
